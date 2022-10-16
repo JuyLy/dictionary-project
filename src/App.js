38eb9b227eaff271cee482df187b9f7d1a9042b8
JuyLy <104866914+JuyLy@users.noreply.github.com>
2022-10-16 17:12:49 +0300

    Added Results.css

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        Dictionary App
+        Dictionary App📖
       </header>
       <div className='container'>
       <Search />
--- src/Meaning.js
+++ src/Meaning.js
@@ -4,7 +4,7 @@ export default function Meaning(props) {
     return (
         <div className="Meaning">
             <h3>
-                {props.meaning.partofSpeech}
+                {props.meaning.partOfSpeech}
             </h3>
             {props.meaning.definitions.map(function(definition, index) {
                 return (
